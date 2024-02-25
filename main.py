3f044c23639fba66b5cf1d5cde6cd7b25ca2cd68
Rikelme Sousa de Carvalho <rikelme.sousa.c@gmail.com>
2024-02-25 17:35:34 -0300

    tirei as bordas

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def futebol():
     janela.destroy()
     futebol = tk.Toplevel()
     global times 
-    times =["PALMEIRAS", "FLAMENGO", "SAOPAULO", "INTERNACIONAL", "GRÊMIO", "CRUZEIRO", "CORINTHIANS", "MANAUS", "BOTAFOGO", "BARCELONA", "LIVERPOOL", "CRB", "GALATASARAY", "VITORIA", "VASCO", "SANTOS", "GOIAS", "FORTALEZA", "PSG", "SEVILLA", "JUVENTUS", "NAPOLI", "FIORENTINA"]
+    times =["PALMEIRAS", "FLAMENGO", "SAOPAULO", "INTERNACIONAL", "GREMIO", "CRUZEIRO", "CORINTHIANS", "MANAUS", "BOTAFOGO", "BARCELONA", "LIVERPOOL", "CRB", "GALATASARAY", "VITORIA", "VASCO", "SANTOS", "GOIAS", "FORTALEZA", "PSG", "SEVILLA", "JUVENTUS", "NAPOLI", "FIORENTINA"]
     global time_escolhido 
     time_escolhido = random.choice(times)
     global erros 
@@ -108,19 +108,20 @@ def futebol():
 
     #Titulo da janela
 
-    titulo = Label(futebol, text="Times de futebol", foreground="#fff", background="#2c6b21", font=("Roboto", 30))
+    titulo = Label(futebol, text="Times de futebol", foreground="#fff", background="#2c6b21", font=("Trebuchet MS", 30))
     titulo.pack(ipady=20)
+
+    #como jogar
+    lbintro1 = Label(futebol, text="Digite letras até completar a palavra, mas lembrando: você tem apenas 3 chances!", fg="#fff", background="#2c6b21", font=("Roboto", 14))
+    lbintro1.pack(anchor="center")
 
     #quadro1 - parte a esquerda da janela
 
-    quadro1 = Frame(futebol, background="#2c6b21", relief="solid", borderwidth=2)
-    quadro1.place(x = 15, y = 150, width = 500, height = 500)
-
-    lbintro1 = Label(quadro1, text="Bem-vindo/a ao jogo da forca(categoria times de futebol)!\n Digite letras até completar a palavra, mas lembrando:\n você tem apenas 3 chances!", fg="#fff", background="#2c6b21", font=("Roboto", 14))
-    lbintro1.pack(pady=20)
+    quadro1 = Frame(futebol, background="#2c6b21")
+    quadro1.place(x = 15, y = 170, width = 500, height = 500)
 
     lbintro2 = Label(quadro1, text="Palavra: {}".format(' '.join(letrasPalavra)), fg="#fff", background="#2c6b21", font=("Roboto", 16))
-    lbintro2.pack()
+    lbintro2.pack(pady=40)
 
 
     lbentry = Label(quadro1, text="Digite aqui a letra: ", fg="#fff", background="#2c6b21", font=("Roboto", 16))
@@ -135,8 +136,8 @@ def futebol():
 
     #quadro2 - parte a direita da janela
 
-    quadro2 = Frame(futebol, background="#2c6b21", relief="solid", borderwidth=2)
-    quadro2.place(x = 530, y = 150, width = 350, height = 500)
+    quadro2 = Frame(futebol, background="#2c6b21", relief="solid")
+    quadro2.place(x = 530, y = 170, width = 350, height = 500)
 
     lbresrodt = Label(quadro2, text="Resultado da rodada:", fg="#fff", background="#2c6b21", font=("Roboto", 20)) #label pra mostrar o titulo resultado da rodada
     lbresrodt.pack(anchor="nw", pady=20, padx=35)
@@ -150,11 +151,13 @@ def futebol():
     lberros = Label(quadro2, text="Quantidade de chances: {}".format(erros), fg="#fff", background="#2c6b21", font=("Roboto", 16)) #label pra mostrar os erros
     lberros.pack(anchor="nw", pady=20, padx=10)
 
+    futebol.mainloop()
+
 def comidas():
     janela.destroy()
     comidas = tk.Toplevel()
     global times 
-    times =["PALMEIRAS", "FLAMENGO", "SAOPAULO", "INTERNACIONAL", "GRÊMIO", "CRUZEIRO", "CORINTHIANS", "MANAUS", "BOTAFOGO", "BARCELONA", "LIVERPOOL", "CRB", "GALATASARAY", "VITORIA", "VASCO", "SANTOS", "GOIAS", "FORTALEZA", "PSG", "SEVILLA", "JUVENTUS", "NAPOLI", "FIORENTINA"]
+    times =["ARROZ", "FEIJAO", "OVO", "MAÇA", "FIGADO", "GALINHA", "BOLO", "AÇAI", "PEQUI", "ABARA", "MACARRAO", "LIMONADA", "SARAPATEL", "LASANHA", "STROGONOFF", "BATATA", "PERA", "UVA", "SUCO", "PAO", "VAGEM", "RABANETE", "RABADA"]
     global time_escolhido 
     time_escolhido = random.choice(times)
     global erros 
@@ -217,7 +220,7 @@ def comidas():
             vit="s"
             redefinir()
         elif erros <= 0:
-            messagebox.showerror(title="Não foi desta vez!", message="Você perdeu! O time era o {}".format(time_escolhido))
+            messagebox.showerror(title="Não foi desta vez!", message="Você perdeu! A comida era {}".format(time_escolhido))
             redefinir()
 
     def redefinir():
@@ -251,19 +254,20 @@ def comidas():
 
     #Titulo da janela
 
-    titulo = Label(comidas, text="Times de futebol", foreground="#fff", background="#2c6b21", font=("Roboto", 30))
+    titulo = Label(comidas, text="Comidas", foreground="#fff", background="#2c6b21", font=("Trebuchet MS", 30))
     titulo.pack(ipady=20)
+
+    #como jogar
+    lbintro1 = Label(comidas, text="Digite letras até completar a palavra, mas lembrando: você tem apenas 3 chances!", fg="#fff", background="#2c6b21", font=("Roboto", 14))
+    lbintro1.pack(anchor="center")
 
     #quadro1 - parte a esquerda da janela
 
-    quadro1 = Frame(comidas, background="#2c6b21", relief="solid", borderwidth=2)
+    quadro1 = Frame(comidas, background="#2c6b21")
     quadro1.place(x = 15, y = 150, width = 500, height = 500)
 
-    lbintro1 = Label(quadro1, text="Bem-vindo/a ao jogo da forca(categoria times de futebol)!\n Digite letras até completar a palavra, mas lembrando:\n você tem apenas 3 chances!", fg="#fff", background="#2c6b21", font=("Roboto", 14))
-    lbintro1.pack(pady=20)
-
     lbintro2 = Label(quadro1, text="Palavra: {}".format(' '.join(letrasPalavra)), fg="#fff", background="#2c6b21", font=("Roboto", 16))
-    lbintro2.pack()
+    lbintro2.pack(pady=40)
 
 
     lbentry = Label(quadro1, text="Digite aqui a letra: ", fg="#fff", background="#2c6b21", font=("Roboto", 16))
@@ -278,7 +282,7 @@ def comidas():
 
     #quadro2 - parte a direita da janela
 
-    quadro2 = Frame(comidas, background="#2c6b21", relief="solid", borderwidth=2)
+    quadro2 = Frame(comidas, background="#2c6b21")
     quadro2.place(x = 530, y = 150, width = 350, height = 500)
 
     lbresrodt = Label(quadro2, text="Resultado da rodada:", fg="#fff", background="#2c6b21", font=("Roboto", 20)) #label pra mostrar o titulo resultado da rodada
@@ -293,9 +297,7 @@ def comidas():
     lberros = Label(quadro2, text="Quantidade de chances: {}".format(erros), fg="#fff", background="#2c6b21", font=("Roboto", 16)) #label pra mostrar os erros
     lberros.pack(anchor="nw", pady=20, padx=10)
 
-
-
-        
+    comidas.mainloop()    
 
 
 def Sobre():
@@ -309,26 +311,29 @@ def Sobre():
     sobre.configure(background="#aad1fc")
 
     #Frame da img
-    Frame_igm = Frame(sobre, background="#aad1fc", relief="solid", borderwidth=2)
+    Frame_igm = Frame(sobre, background="#aad1fc")
     Frame_igm.place(x =40, y = 100, width= 300, height= 400)
     #foto
-    image = Image.open("C:/Users/rikel/OneDrive/Documentos/MeusProjetos/JogodaForca/joia.gif")
-    photo = ImageTk.PhotoImage(image)
-    img = Label(Frame_igm, image=photo)
-    img.pack()
+    try: 
+        image = Image.open("C:/Users/rikel/OneDrive/Documentos/MeusProjetos/JogodaForca/joia.gif")
+        photo = ImageTk.PhotoImage(image)
+        img = Label(Frame_igm, image=photo)
+        img.pack(pady=50)
+    except EXCEPTION as erro:
+        print(erro)
 
 
     #Frame texto
-    Frame_txt = Frame(sobre, background="#aad1fc", relief="solid", borderwidth=2)
+    Frame_txt = Frame(sobre, background="#aad1fc")
     Frame_txt.place(x = 370, y = 100, width= 500, height= 400)
     #Parágrafos
-    h1 = Label(Frame_txt,text="Obrigado!", font=("Montserrat", 30), background="#aad1fc")
-    h1.pack(anchor="nw", padx=10, pady=20)
-    p1 = Label(Frame_txt,text="Agradeço pelo interesse e pela avaliação do jogo", font=("Montserrat",16), background="#aad1fc")
+    h1 = Label(Frame_txt,text="Obrigado!", font=("Trebuchet MS", 30), background="#aad1fc")
+    h1.pack(anchor="nw", padx=10, pady=60)
+    p1 = Label(Frame_txt,text="Agradeço pelo interesse e pela avaliação do jogo!", font=("Segoe UI",16), background="#aad1fc")
     p1.pack(anchor="nw", padx=10)
-    p2 = Label(Frame_txt,text="Desenvolvido por Rikelme Sousa de Carvalho", font=("Montserrat",16), background="#aad1fc")
+    p2 = Label(Frame_txt,text="Desenvolvido por Rikelme Sousa de Carvalho", font=("Segoe UI",16), background="#aad1fc")
     p2.pack(anchor="nw", padx=10)
-
+    sobre.mainloop()
 
 
 janela = Tk()
@@ -341,7 +346,7 @@ janela.iconbitmap(icone)
 janela.geometry("900x700")
 janela.configure(background="#2c6b21")
 
-titulo = Label(janela, text="Jogo da forca", foreground="#fff", background="#2c6b21", font=("Roboto", 32))
+titulo = Label(janela, text="Jogo da forca", foreground="#fff", background="#2c6b21", font=("Trebuchet MS", 32))
 titulo.pack(ipady=20)
 
 main = Frame(janela, background="#2c6b21")
